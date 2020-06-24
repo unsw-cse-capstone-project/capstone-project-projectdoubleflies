109ce68515798c98f7f6235db7be85ea5fb074e3
danting123 <47726688+danting123@users.noreply.github.com>
2020-06-24 23:44:16 +1000

    Ingredient changed

---
+++ Ingredient.java
@@ -0,0 +1,53 @@
+package cs.model;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+@Embeddable
+public class Ingredient {
+	@NotNull
+	@NotEmpty
+	private String ingredient;
+	@NotNull
+	@NotEmpty
+	private String category;
+	@NotNull
+	@NotEmpty
+	private String amount;
+	
+	public Ingredient() {}
+	
+	public Ingredient(String ingredient, String category, String amount) {
+		// TODO Auto-generated constructor stub
+		this.amount = amount;
+		this.ingredient = ingredient;
+		this.category = category;
+	}
+	public String getIngredient() {
+		return ingredient;
+	}
+	public void setIngredient(String ingredient) {
+		this.ingredient = ingredient;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return this.ingredient+" "+this.amount+" "+this.category;
+	}
+
+}
