8b1c299ef9b80f7f4fe104553a29e6dd94393f2e
Le Anh Khoa Nguyen <z5189020@ad.unsw.edu.au>
2020-06-27 23:18:20 +1000

    Backend for Authentication

---
+++ system/cs/src/main/java/cs/model/AuthenticationController.java
@@ -0,0 +1,36 @@
+package cs.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cs.model.UserService;
+
+@Controller
+public class AuthenticationController {
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/login")
+	@ResponseBody
+	public boolean login(@RequestParam String username, @RequestParam String password) {
+		if (userService.loginSuccessful(username, password) == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@PostMapping("/register")
+	@ResponseBody
+	public boolean register(@RequestParam String username, @RequestParam String password) {
+		if (userService.registerSuccessful(username, password) == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ system/cs/src/main/java/cs/model/UserService.java
@@ -0,0 +1,32 @@
+package auth.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cs.model.User;
+import cs.model.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public boolean loginSuccessful(String username, String password){
+		User user = userRepository.findByUsername(username);
+		if (user != null) {
+			if (user.getPassword().equals(password))
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean registerSuccessful(String username, String password){
+		User user = userRepository.findByUsername(username);
+		if (user == null) {
+			userRepository.save(new User(username, password));
+			return true;
+		}
+		return false;
+	}
+}
