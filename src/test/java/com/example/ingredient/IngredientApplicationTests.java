6cbd944c02adb035e93da969afdbc9b64755084f
breezyhy <53144075+breezyhy@users.noreply.github.com>
2020-06-29 18:58:37 +1000

    Add files via upload

---
+++ src/test/java/com/example/ingredient/IngredientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.ingredient;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
