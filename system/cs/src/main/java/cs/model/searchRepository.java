d845f1703fd96d5186795dfa8c6c15ed7f3db351
danting123 <47726688+danting123@users.noreply.github.com>
2020-07-02 17:00:58 +1000

    search repo changed

--- system/cs/src/main/java/cs/model/searchRepository.java
+++ system/cs/src/main/java/cs/model/searchRepository.java
@@ -8,5 +8,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface searchRepository extends JpaRepository<SearchHistory, Long>{
+    @Query(value="SELECT s.ingredient from searchistory_info s WHERE s.searchid=:searchid",nativeQuery=true)
+    List<String> history(@Param("searchid") Long searchid);
 	
+    @Query("select s from SearchHistory s where s.searchID=?1")
+    SearchHistory test(Long id);
+    
+    @Query(value = "select * from search_history order by frequency desc", nativeQuery=true)
+    List<SearchHistory> help();
 }
