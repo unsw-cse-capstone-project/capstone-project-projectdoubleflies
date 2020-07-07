14712b1aed70bdccdf5fee9873dfb5068995d135
danting123 <47726688+danting123@users.noreply.github.com>
2020-07-07 22:47:31 +1000

    searchHistory change

--- system/cs/src/main/java/cs/model/SearchHistory.java
+++ system/cs/src/main/java/cs/model/SearchHistory.java
@@ -4,7 +4,22 @@ import javax.persistence.*;
 
 @Entity
 public class SearchHistory {
+    @Converter
+    public static class StringListConverter implements AttributeConverter<List<String>, String> {
+
+		@Override
+		public String convertToDatabaseColumn(List<String> attribute) {
+			// TODO Auto-generated method stub
+			return String.join(",", attribute); 
+		}
+
+		@Override
+		public List<String> convertToEntityAttribute(String dbData) {
+			// TODO Auto-generated method stub
+			return new ArrayList<>(Arrays.asList(dbData.split(",")));
+		}
 		
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long searchID;
@@ -19,6 +34,11 @@ public class SearchHistory {
   //  @MapKeyJoinColumn(name="searchID2")
     //@Column(name="frequency3")
     private Integer frequency;
+    /*-------------------Test---------------------------------------------------*/
+    @Column
+    @Convert(converter = StringListConverter.class)
+    @JsonIgnore
+    private List<String> meal;
 	    
     public SearchHistory() {}
 
@@ -56,6 +76,15 @@ public class SearchHistory {
 
 	public void setFrequency(Integer frequency) {
 		this.frequency = frequency;
+	}
+	
+	@JsonIgnore
+	public List<String> getMeal() {
+		return meal;
+	}
+
+	public void setMeal(List<String> meal) {
+		this.meal = meal;
 	}
 	
 	
