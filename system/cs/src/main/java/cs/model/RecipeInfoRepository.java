9b945230b3f0f325543168564c8745fe7fc70be7
danting123 <47726688+danting123@users.noreply.github.com>
2020-07-09 23:04:39 +1000

    Update RecipeInfoRepository.java

--- system/cs/src/main/java/cs/model/RecipeInfoRepository.java
+++ system/cs/src/main/java/cs/model/RecipeInfoRepository.java
@@ -41,7 +41,21 @@ public interface RecipeInfoRepository extends JpaRepository<Recipe, Integer> {
     @Query(value="SELECT  FROM ingredient_info i",nativeQuery=true)
     List<Ingredient> search();
     
-    @Query(value="select * from recipe where recipeid IN (select recipeid from (select r.recipeid, count(distinct i.ingredient) as ct from recipe r join ingredient_info i on r.recipeid=i.recipeid join (select i2.ingredient from ingredient_info i2 where i2.ingredient IN :ingredients) as t on i.ingredient=t.ingredient group by r.recipeid having count(distinct i.ingredient)>=(select count(distinct i3.ingredient) from ingredient_info i3 where i3.ingredient IN :ingredients) ORDER BY ct) as t)", nativeQuery = true)
-    List<Recipe> ing(@Param("ingredients") List<String> ingredients);
+  @Query(value="select * from Recipe where recipeid IN (select recipeid from (select r.recipeid, "
+    		+ "count(distinct i.ingredient) as ct from Recipe r join ingredient_info i on r.recipeid=i.recipeid "
+    		+ "join (select i2.ingredient from ingredient_info i2 where i2.ingredient IN :ingredients) as t "
+    		+ "on i.ingredient=t.ingredient group by r.recipeid having "
+    		+ "count(distinct i.ingredient)>=(select count(distinct i3.ingredient) "
+    		+ "from ingredient_info i3 where i3.ingredient IN :ingredients) ORDER BY ct) as t) and type=:types", nativeQuery = true)
+    List<Recipe> ing(@Param("types") String type, @Param("ingredients") List<String> ingredients);
+    
+    
+    @Query(value="select * from Recipe where recipeid IN (select recipeid from (select r.recipeid, "
+    		+ "count(distinct i.ingredient) as ct from Recipe r join ingredient_info i on r.recipeid=i.recipeid "
+    		+ "join (select i2.ingredient from ingredient_info i2 where i2.ingredient IN :ingredients) as t "
+    		+ "on i.ingredient=t.ingredient group by r.recipeid having "
+    		+ "count(distinct i.ingredient)>=(select count(distinct i3.ingredient) "
+    		+ "from ingredient_info i3 where i3.ingredient IN :ingredients) ORDER BY ct) as t)", nativeQuery = true)
+    List<Recipe> filter(@Param("ingredients") List<String> ingredients);
 
 }
