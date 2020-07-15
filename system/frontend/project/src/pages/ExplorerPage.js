dc47d6968859f94c3d3b3acc941fc7759207cf09
KyrieeWen <41510801+KyrieeWen@users.noreply.github.com>
2020-07-15 22:25:00 +1000

    add background image for explorer page

--- system/frontend/project/src/pages/ExplorerPage.js
+++ system/frontend/project/src/pages/ExplorerPage.js
@@ -3,7 +3,8 @@ import ListRecipe from './ListRecipe'
 import { FAVOURITE } from '../helpers/type'
 import { connect } from 'react-redux';
 import { checkLoggedIn } from '../actions/userActions'
-import { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom'; 
+import Background from '../background.jpg'
 
 
 class ExplorerPage extends Component {
@@ -11,6 +12,7 @@ class ExplorerPage extends Component {
 		
 		return (
 			<>
+			<img src={Background} alt="" className="bg"/>
 			<h4>Favourite Recipes</h4>
 			{this.props.loggedIn && <ListRecipe/>}
 			{!this.props.loggedIn && <Redirect to="/"/>}
